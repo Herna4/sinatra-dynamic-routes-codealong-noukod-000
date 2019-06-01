320a780de8f3d52c5829c362c29f5a0170536a66
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-01 01:38:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,13 +15,14 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
   
-  # get '/medicines/:id'
-  # end
    # medicines_controller.rb
   get '/medicines/:id' do
     @medicine = all_the_medicines.select do |medicine|
       medicine.id == params[:id]
   end.first
     erb :'/medicines/show.html'
+  end
+  
+  get '/medicines/:id'
   end
 end
