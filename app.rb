e921a1eb6812d0e793c2dea56816adf445c3b882
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-01 01:55:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,12 +18,14 @@ class App < Sinatra::Base
   get '/goodbye/:name' do
     # @user_name = params[:name]
     # "Goodbye, #{@user_name}."
+    erb :goodbye
   end
 
   get '/multiply/:num1/:num2' do
     # @num1 = params[:num1].to_i
     # @num2 = params[:num2].to_i
     # "#{@num1 * @num2}"
+    erb :multiply
   end
 
 end
